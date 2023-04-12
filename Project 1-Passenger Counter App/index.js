8c05bac005d2e331e0e55f934831af962ffadd5f
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2023-04-12 09:27:06 -0500

    Add files via upload

---
+++ Project 1-Passenger Counter App/index.js
@@ -0,0 +1,21 @@
+//document.getElementById('count-el').innerText = 5;
+
+//win+k+c - comment multiple lines
+// let firstBatch = 5;
+// let secondBatch = 7;
+// let count = firstBatch + secondBatch;
+// console.log(count);
+
+//Throws an error since we are trying to access the variable before it is declared
+//console.log(myAge);
+//let myAge = 30;
+
+
+//1. Create two variables, myAge and humanDogRatio
+//2. Multiply the two together and store the result in myDogAge
+//3. Log myDogAge to the console
+
+let myAge = 30;
+let humanDogRatio = 7;
+let myDogAge = myAge * humanDogRatio;
+console.log(myDogAge);
