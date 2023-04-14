093b84bbf6c62ee6b3179e2f9e07c97aa6ef53fa
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2023-04-14 09:38:04 -0500

    Add files via upload

--- Project 1-Passenger Counter App/index.js
+++ Project 1-Passenger Counter App/index.js
@@ -1,5 +1,20 @@
 //document.getElementById('count-el').innerText = 5;
 
+// let count = 5;
+// count = count +
+// console.log(count);
+
+// initialize the count as 0
+//listen for clicks on the increment button
+//increment the count variable when the button is clicked
+//chang the count-el in the HTML to reflect the new count
+
+function increment() {
+    console.log("The button was clicked");
+}
+
+// |||||||ADDITIONAL EXERCISES AS PART OF TUTORIAL|||||||
+
 //win+k+c - comment multiple lines
 // let firstBatch = 5;
 // let secondBatch = 7;
@@ -15,7 +30,41 @@
 //2. Multiply the two together and store the result in myDogAge
 //3. Log myDogAge to the console
 
-let myAge = 30;
-let humanDogRatio = 7;
-let myDogAge = myAge * humanDogRatio;
-console.log(myDogAge);
+// let myAge = 30;
+// let humanDogRatio = 7;
+// let myDogAge = myAge * humanDogRatio;
+// console.log(myDogAge);
+
+//Create a variable, bonusPoints. Intialize it as 50. Increase it to 100.
+//Decrease it down to 25, and then finally increase it to 70
+//Console.log the value after each step
+
+// let bonusPoints = 50;
+// console.log(bonusPoints);
+
+// bonusPoints = bonusPoints + 50;
+// console.log(bonusPoints);
+
+// bonusPoints = bonusPoints - 75;
+// console.log(bonusPoints);
+
+// bonusPoints = bonusPoints + 45;
+// console.log(bonusPoints);
+
+// RACE CAR FUNCTION - Using Functions to Write Less Code
+// function countdown() {
+//     console.log(5);  
+//     console.log(4);  
+//     console.log(3);  
+//     console.log(2);  
+//     console.log(1); 
+// }
+// // Setting up the race 
+// countdown();
+
+// // GO!
+// // Players are running the race
+// //Race is finished!
+
+// //Get ready for a new race
+// countdown();
