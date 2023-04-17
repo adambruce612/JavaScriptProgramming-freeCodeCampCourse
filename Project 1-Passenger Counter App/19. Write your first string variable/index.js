79cba33833057fe805ac0c91bc26a2f1f6fd6103
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2023-04-17 09:35:19 -0500

    Add files via upload

---
+++ Project 1-Passenger Counter App/19. Write your first string variable/index.js
@@ -0,0 +1,10 @@
+let username = 'per';
+
+//Create a variable, message, that stores the string "You have three new notifications"
+let message = "You have three new notifications";
+
+console.log(message + ', ' + username + '!');
+
+//Create a variable, messageToUser, that contains the message we have logged
+let messageToUser = message + ', ' + username + '!';
+console.log(messageToUser);
