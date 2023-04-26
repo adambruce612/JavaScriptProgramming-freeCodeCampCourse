243eaa92074edb6a496182d17788e6893fdc531e
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2023-04-26 09:56:12 -0500

    Add files via upload

---
+++ 3. Build a Blackjack Game/22. Array indexes/index.js
@@ -0,0 +1,21 @@
+// Arrays - ordered lists of items
+
+//0 indexed
+let featuredPosts = [
+    "Check out my Netflix clone", //0
+    "Here's the code for my project", //1
+    "I've just relaunched my portfolio" //2
+]
+
+console.log(featuredPosts.length); //length is not zero indexed 
+
+// let experience = ["CEO at Scrimba", "Frontend developer at Xeneta", "People counter for Norstat"]
+
+// // Make the following appear in the console:
+// // Frontend developer at Xeneta
+// // People counter for Norstat
+// // CEO at Scrimba
+
+// console.log(experience[1]);
+// console.log(experience[2]);
+// console.log(experience[0]);
