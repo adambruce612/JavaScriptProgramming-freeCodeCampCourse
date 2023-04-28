4456fc5e8f0f4f980f94016feaf9602f72b0803d
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2023-04-28 10:03:22 -0500

    Add files via upload

---
+++ 3. Build a Blackjack Game/36. Generating random numbers with Math.random()/index.js
@@ -0,0 +1,15 @@
+let randomNumber = Math.random()
+
+console.log(randomNumber)
+
+
+/* 
+
+What does Math.random() do?
+
+Your answer: Math.random() generates a random number between 0 and 1 (not inclusive of 1). 
+The MDN definition: The Math.random() static method returns a floating-point, pseudo-random number that's greater than or equal to 0 and less than 1, 
+with approximately uniform distribution over that range — which you can then scale to your desired range. The implementation selects the initial seed 
+to the random number generation algorithm; it cannot be chosen or reset by the user.
+
+*/
---
+++ 3. Build a Blackjack Game/38. Flooring the number with Math.floor()/index.js
@@ -0,0 +1,14 @@
+// let randomNumber = Math.random() * 6
+
+let flooredNumber = Math.floor(3.45632)
+
+console.log(flooredNumber)
+
+
+/* 
+
+What does Math.floor() do to positive numbers? It removes the decimals  (it always rounds down)
+
+Your answer: Math.floor() always rounds down and returns the largest integer less than or equal to a given number (MDN definition).
+
+*/
