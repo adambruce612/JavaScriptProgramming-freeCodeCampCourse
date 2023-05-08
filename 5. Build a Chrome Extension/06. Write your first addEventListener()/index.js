e25327e35a4a39e88f3d310ba093c654c4ce12d4
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2023-05-08 09:44:53 -0500

    Add files via upload

---
+++ 5. Build a Chrome Extension/06. Write your first addEventListener()/index.js
@@ -0,0 +1,9 @@
+// 1. Grab the box from the DOM and store it in a variable
+let box = document.getElementById("box");
+
+// 2. Add a click event listener to the box 
+box.addEventListener('click', function() {
+// 3. Log out "I want to open the box!" when it's clicked
+    console.log("I want to open the box!");
+})
+
